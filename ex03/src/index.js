5a7cde4ccefa55745bff389312929dd95eff3421
Sumedin Bilalic <sumko87@gmail.com>
2022-04-04 13:20:21 +0200

    working with local scope variables

---
+++ ex03/src/index.js
@@ -0,0 +1,16 @@
+// Write code below this line
+function localScope() {
+    var myVariable = "I am here!";
+    console.log(myVariable);
+}
+// Write code above this line
+
+// myVariable is not defined outside of localScope
+localScope();
+
+if(typeof myVariable!="undefined") {
+    console.log('outside localScope', myVariable);
+} else {
+    console.log('outside localScope UNDEFINED!!!');
+}
+module.exports = localScope;
